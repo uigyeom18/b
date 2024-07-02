8af507525056adcce366abeaac1ca47c489fa34d
uigyeom18 <kajar347@gmail.com>
2024-07-02 09:41:41 +0900

    진짜완료

--- suika33/main.js
+++ suika33/main.js
@@ -5,7 +5,9 @@ var Engine = Matter.Engine,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
     World = Matter.World,
-    Body = Matter.Body;
+    Body = Matter.Body,
+    Events = Matter.Events;
+
 
     //엔진 선언
     const engine = Engine.create();
@@ -38,6 +40,7 @@ var Engine = Matter.Engine,
         render : { fillStyle: '#E6B143'} // 색상지정
     })
     const topLine = Bodies.rectangle(310,150,620,2,{
+        name : "topLine",
         isStatic : true, // 고정해주는 기능
         isSensor : true,
         render : { fillStyle: '#E6B143'} // 색상지정
@@ -86,12 +89,14 @@ var Engine = Matter.Engine,
 
         switch(event.code){
             case "KeyA":
+                if(currentBody.position.x - currentFruit.radius>30)
                 Body.setPosition(currentBody, {
                     x : currentBody.position.x -10,
                     y : currentBody.position.y
                 })
                 break;
             case "KeyD":
+                if(currentBody.position.x + currentFruit.radius<590)
                 Body.setPosition(currentBody, {
                     x : currentBody.position.x +10,
                     y : currentBody.position.y
@@ -108,5 +113,42 @@ var Engine = Matter.Engine,
                 break;
         }
     }
+    Events.on(engine, "collisionStart",(event)=>{
+        //콜리전 이벤트 발생시 생기는 모든 오브젝트를 비교
+        event.pairs.forEach((collision)=>{
+            if(collision.bodyA.index == collision.bodyB.index){
+                //기존 과일의 index를 저장
+                const index = collision.bodyA.index;
+
+                //수박일 경우 처리 안함
+                if(index === FRUITS.length - 1)
+                    return;
+
+                //충돌이 일어나는 같은 과일 제거
+                World.remove(world, [collision.bodyA, collision.bodyB]);
+
+                //기존 과일에서 1 증가 시킨 값을 저장
+                const newFruit = FRUITS[index + 1];
+                const newBody = Bodies.circle(
+                    //부딪친 위치의 x,y값
+                    collision.collision.supports[0].x,
+                    collision.collision.supports[0].y,
+
+                    newFruit.radius,
+                    {
+                        index : index + 1,
+                        render : {sprite : { texture : `${newFruit.name}.png`}},
+                    }
+                );
+                World.add(world,newBody);
+              
+            }
+            if(!disableAction && (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")){
+                alert("game over")
+                disableAction = true;
+            }
+        })
+    });
+
     addFruit();
     
